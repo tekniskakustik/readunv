a6fc6e20cb0c5a76f2fd4d31c6eb5e6ce70a3907
lars-whta <lars@acoustics.tech>
2022-04-27 10:10:49 +0200

    init

--- compile_mex.m
+++ compile_mex.m
@@ -3,8 +3,8 @@
 % Tested with Intel Fortran Compiler (ifort) version 2021.5.0, Visual Studio 2019, Windows x64
 % NB: -R2018a flag is required
 if ispc
-    mex -R2018a COMPFLAGS='$COMPFLAGS /warn:unused /Os /Qax:AVX2' readunv.F 
-    mex -R2018a COMPFLAGS='$COMPFLAGS /warn:unused /Os /Qax:AVX2' writeunv.F
+    mex -R2018a COMPFLAGS='$COMPFLAGS /warn:unused /Os /Qax:AVX2 /Qunroll-aggressive' readunv.F
+    mex -R2018a COMPFLAGS='$COMPFLAGS /warn:unused /Os /Qax:AVX2 /assume:buffered_io /Qunroll-aggressive' writeunv.F
 elseif ismac
 
 else % linux
