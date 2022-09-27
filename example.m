ef43959bc4b562eb4f41b3f9745a105d7f0eb9dd
lars-whta <lars@acoustics.tech>
2022-09-27 15:08:00 +0000

    bugfixes streaming write

--- example.m
+++ example.m
@@ -107,6 +107,7 @@ end
 writeunv(); % close all files
 
 
+% single-precision streaming
 for stp = 1:42 % try streaming with different step sizes
 
     % write header data to new file, but keep file open between calls
@@ -122,7 +123,44 @@ for stp = 1:42 % try streaming with different step sizes
     [success, casenum] = writeunv([], S, 2, idx); % 58 header
     L = size(data, 1);
     
-    % writeunv();
+    for ii = 1:ceil(L/stp)
+        dataToWrite = data(1+(ii-1)*stp:min(ii*stp, end)); 
+        y = writeunv(casenum, dataToWrite, 3, idx, 58);
+    end
+    writeunv(-1, [], 3, idx, 58); % terminate file
+    % close all files is not needed here
+    
+
+    fid = fopen(filepath, 'rb');
+    fseek(fid, 0, 'eof');
+    fileSize = ftell(fid);
+    frewind(fid);
+    data = fread(fid, fileSize, 'uint8');
+    numLines = sum(data == 10) + 1; % check number of lines
+    disp([stp, numLines])
+    fclose(fid);
+
+end
+
+
+% double-precision streaming
+for stp = 1:21 % try streaming with different step sizes
+
+    % write header data to new file, but keep file open between calls
+    % add data (58) from one set in chunks (streaming) and terminate when done
+    idx = 5; % index number to use between calls, in order to keep multiple files open simultaneously, any number between 1 and 512 (MAXIDX)
+    filepath = ['.', filesep, 'example4.unv'];
+    writeunv(filepath, datacell{1}, 1, idx); % 151 header
+    writeunv([], datacell{2}, 2, idx); % 164 header
+    S = datacell{3};
+    S.numValues = 0; % remove correct value, for demonstration purposes
+    data = double(S.data);
+    S.dataFormatType = 4; % 4 = double, 2 = single | required since no data is sent when writing header
+    S.precision = 'DOUBLE'; % not needed
+    S = rmfield(S, 'data'); % remove data
+    [success, casenum] = writeunv([], S, 2, idx); % 58 header
+    L = size(data, 1);
+    
     for ii = 1:ceil(L/stp)
         dataToWrite = data(1+(ii-1)*stp:min(ii*stp, end)); 
         y = writeunv(casenum, dataToWrite, 3, idx, 58);
