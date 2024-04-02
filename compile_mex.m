b41695885434488e3a6dc90e0be194ed498a7b50
lars-whta <lars@acoustics.tech>
2024-04-02 20:00:27 +0200

    2024a

--- compile_mex.m
+++ compile_mex.m
@@ -1,7 +1,7 @@
 
 
 % Tested with:
-% Windows: Matlab R2023a update 6, Intel Fortran Compiler (ifort) version 2021.10.0, Visual Studio 2022 (17.8.4), Windows 11 22H2 Build 23619
+% Windows: Matlab R2024a update 0, Intel Fortran Compiler (ifort) version 2021.12.0, Visual Studio 2022 (17.9.1), Windows 11 24H2 Build 26090
 % Linux:   Matlab R2023a update 6, Intel Fortran Compiler (ifort) version 2021.10.0, GCC 13.2.1                   Clear Linux 40680
 % Mac:     Matlab R2022b update 0, Intel Fortran Compiler (ifort) version 2021.7.0,  Xcode 14.0.1,                Macos 12.6
 
@@ -9,17 +9,17 @@
 % NB: -R2018a flag is required
 if ispc
     
-    mex -R2018a COMPFLAGS='$COMPFLAGS /warn:unused /O2' readunv.F .\source\read\*.F -output readunv
-    mex -R2018a COMPFLAGS='$COMPFLAGS /warn:unused /O2 /QaxAVX2 /assume:buffered_io /Qunroll-aggressive' writeunv.F .\source\write\*.F  -output writeunv
+    mex -R2018a COMPFLAGS='$COMPFLAGS /Qdiag-disable:10448 /warn:unused /O2 /QaxAVX2 /assume:buffered_io /Qunroll-aggressive' readunv.F .\source\read\*.F -output readunv
+    mex -R2018a COMPFLAGS='$COMPFLAGS /Qdiag-disable:10448 /warn:unused /O2 /QaxAVX2 /assume:buffered_io /Qunroll-aggressive' writeunv.F .\source\write\*.F  -output writeunv
 
 elseif ismac
 
-    mex -R2018a FOPTIMFLAGS='$FOPTIMFLAGS -warn unused -O2' readunv.F ./source/read/*.F -output readunv
+    mex -R2018a FOPTIMFLAGS='$FOPTIMFLAGS -warn unused -O2 -axAVX2 -assume buffered_io -unroll-aggressive' readunv.F ./source/read/*.F -output readunv
     mex -R2018a FOPTIMFLAGS='$FOPTIMFLAGS -warn unused -O2 -axAVX2 -assume buffered_io -unroll-aggressive' writeunv.F ./source/write/*.F  -output writeunv
 
 else % linux
 
-    mex FC='ifort' -R2018a FOPTIMFLAGS='$FOPTIMFLAGS -O2 -i8' readunv.F ./source/read/*.F -output readunv
+    mex FC='ifort' -R2018a FOPTIMFLAGS='$FOPTIMFLAGS -O2 -i8 -axAVX2 -assume buffered_io -unroll-aggressive' readunv.F ./source/read/*.F -output readunv
     mex FC='ifort' -R2018a FOPTIMFLAGS='$FOPTIMFLAGS -O2 -i8 -axAVX2 -assume buffered_io -unroll-aggressive' writeunv.F ./source/write/*.F  -output writeunv
 
 end
